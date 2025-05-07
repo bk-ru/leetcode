d915138c9769b12bec26a38714be34f0faab8405
Evgeni Anisimov <batyrkotiev0@mail.ru>
2025-05-07 22:12:43 +0300

    .\.git\

--- Medium/Product of Array Except Self/main.cpp
+++ Medium/Product of Array Except Self/main.cpp
@@ -4,10 +4,30 @@
 class Solution {
     public:
         std::vector<int> productExceptSelf(std::vector<int>& nums) {
-            
+            std::vector<int> result = {};
+
+            int mult = 1;
+            for (int i : nums) {
+                result.push_back(mult);
+                mult *= i;
+            }
+
+            mult = 1;
+            for (int i = nums.size() - 1; i >= 0; i--) {
+                result[i] *= mult;
+                mult *= nums[i];
+            }
+
+            return result;
         }
     };
 
 int main(void) {
+    std::vector<int> nums = {1,2,3,4};
 
+    Solution solution;
+
+    for (int i : solution.productExceptSelf(nums)) {
+        std::cout << i << ", ";
+    }
 }
