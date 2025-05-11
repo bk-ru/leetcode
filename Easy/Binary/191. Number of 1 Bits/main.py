670cc708e1d48fe88c348c905c59fcc005053c17
Evgeni Anisimov <batyrkotiev0@mail.ru>
2025-05-11 19:10:15 +0300

    method - bit, module

--- Easy/Binary/191. Number of 1 Bits/main.py
+++ Easy/Binary/191. Number of 1 Bits/main.py
@@ -1,3 +1,13 @@
+# class Solution:
+#     def hammingWeight(self, n: int) -> int:
+#         count = 0
+        
+#         while n > 0:
+#             if n & 1 == 1:
+#                 count += 1
+#             n = n >> 1
+#         return count
+
 class Solution:
     def hammingWeight(self, n: int) -> int:
         tmp = n
